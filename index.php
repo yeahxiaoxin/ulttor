04f1555f69a032ac86f1387dfc9eacd712f2de10
yeahxiaoxin <34412829+yeahxiaoxin@users.noreply.github.com>
2017-12-13 16:12:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+use \LeanCloud\Client;
+// 参数依次为 AppId, AppKey, MasterKey
+Client::initialize("vWfTdVkt2d0C3sJ6Wy1h123V-gzGzoHsz", "qyJfdXdN8udwFJ88LlOwc9gc", "D1zYOe90Gz040naN9hgKPyYu");
+
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',true);
